886b26f558ef9a282386116e00c4788ffd9f7955
Nikita Sharma <snikita312@gmail.com>
2020-07-10 19:19:39 +0530

    Add forecast information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (longitude, latitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=ee70b1e80a2d6143c23f2640ef364791&query=' + longitude + ',' + latitude + '&units=f'
     request({url,json:true},(error,{body})=>{
@@ -9,7 +11,7 @@ const forecast = (longitude, latitude, callback) => {
         }else if(body.error){
             callback('Unable to find location')
         }else{
-            callback(undefined,body.current.weather_descriptions[0] + ". It is currently " +  body.current.temperature + " feels like " + body.current.feelslike)
+            callback(undefined,body.current.weather_descriptions[0] + ". It is currently " +  body.current.temperature + " feels like " + body.current.feelslike + ". Humidity is " + body.current.humidity)
         }
     })
 }
