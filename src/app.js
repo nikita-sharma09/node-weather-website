86b7e69e1261d434e877b3776eee196fb56b92da
Nikita Sharma <snikita312@gmail.com>
2020-07-10 18:55:41 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define Paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -105,6 +106,6 @@ app.get('*',(req,res) => {
     res.send('My 404 page')
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
